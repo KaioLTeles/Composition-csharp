c6affe69d1d76a57b87c6d0d5d4baf7844f1979e
Kaio Teles <kaiolunateles@gmail.com>
2019-07-25 17:11:35 -0300

    Adding project in the remote repository.

---
+++ ExerciciosCurso/Entities/Order.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+using ExerciciosCurso.Entities.Enums;
+using ExerciciosCurso.Entities;
+namespace ExerciciosCurso.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus orderStatus, Client client)
+        {
+            Moment = moment;
+            OrderStatus = orderStatus;
+            Client = client;
+        }
+
+        public void Additem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void Removeitem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double result = 0.0;
+
+            foreach(OrderItem item in Items)
+            {
+                result += item.SubTotal();
+            }
+
+            return result;
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Order moment: " + Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine("Order status: " + OrderStatus);
+            sb.AppendLine("Client: " + Client);
+            sb.AppendLine("Order Items");
+            foreach(OrderItem item in Items)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            sb.AppendLine("Total price: $" + Total().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+        }
+    }
+}
---
+++ ExerciciosCurso/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using ExerciciosCurso.Entities;
+using ExerciciosCurso.Entities.Enums;
+
+namespace ExerciciosCurso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter cliente data");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine()); 
+
+            Client client = new Client(name, email, birthDate);
+
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Order order = new Order
+            {
+                Moment = DateTime.Now,
+                OrderStatus = status,
+                Client = client
+
+            };
+
+            Console.Write("How many items to this order? ");
+            int number = int.Parse(Console.ReadLine());
+
+            for(int i =0; i < number; i++)
+            {
+
+
+                Console.WriteLine($"Enter #{(i+1)} item data");
+                Console.Write("Product Name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double productPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: " );         
+                int qnt = int.Parse(Console.ReadLine());
+
+                Product product = new Product(productName, productPrice);
+
+                OrderItem item = new OrderItem(qnt, productPrice, product);
+
+                order.Additem(item);
+            }
+
+            Console.WriteLine("ORDER SUMMARY:");
+
+            Console.WriteLine(order);
+        }
+    }
+}
